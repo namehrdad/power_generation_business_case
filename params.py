404d1c6bdacfe57a7c1259a52f40ad54aded6b22
namehrdad <99048182+namehrdad@users.noreply.github.com>
2025-08-13 14:40:05 -0700

    params.py

--- params.py
+++ params.py
@@ -4,23 +4,23 @@
 PLANT_SIZE_MW = 100  # Plant size in MW
 NUMBER_OF_GENERATORS = 10
 GENERATOR_MODEL = "20V31SG"
-ANALYSIS_PERIOD_YEARS = 25
+ANALYSIS_PERIOD_YEARS = 20
 
 # Unit conversions (we'll use these with pint later)
 MILLION = 1_000_000
 
 # Cost Categories (in millions of dollars)
-EQUIPMENT_COST = 150      # Total equipment cost in million $
+EQUIPMENT_COST = 102      # Total equipment cost in million $
 INSTALLATION_FACTOR = 3.0     # Installation cost = equipment cost * this factor
-LAND_COST = 10.0             # Land cost in million $
+LAND_COST = 5.0             # Land cost in million $
 
 # Operational Parameters
-CAPACITY_FACTOR = 0.9  # How often the plant runs (85%)
+CAPACITY_FACTOR = 1  # How often the plant runs (85%)
 HOURS_PER_YEAR = 8760
-EFFICIENCY = 0.45 # Efficiency of the generators (45%)
+EFFICIENCY = 0.40 # Efficiency of the generators (45%)
 
 # Operating Costs (annual, in base year dollars)
-OM_COST_ANNUAL = 5.0           # O&M cost in million $ per year
+OM_COST_ANNUAL =2.0           # O&M cost in million $ per year
 
 # Natural Gas Costs
 NATURAL_GAS_PRICE = 4.26       # $ per GJ
@@ -36,10 +36,11 @@ MAJOR_MAINTENANCE_YEARS = [3, 6, 9, 12, 15, 18]  # Years when major maintenance 
 MAJOR_MAINTENANCE_COST_PERCENT = 0.10  # 10% of equipment cost
 
 # Grid Electricity (baseline comparison)
-CONNECTION_INSTALLATION_COST = 5 # MIL$ - for grid connection equipment
+CONNECTION_INSTALLATION_COST = 15 # MIL$ - for grid connection equipment
 GRID_ELECTRICITY_RATE = 0.06   # $ per kWh from grid
 GRID_DEMAND_CHARGE = 10.0      # $ per kW per month
 
 # Financial Parameters
-INFLATION_RATE = 0.03    # 3% annual inflation affecting all future costs
-DISCOUNT_RATE = 0.05     # 8% discount rate for NPV calculations
+INFLATION_RATE = 0.05    # 3% annual inflation affecting all future costs
+DISCOUNT_RATE = 0.063     # 8% discount rate for NPV calculations
+
